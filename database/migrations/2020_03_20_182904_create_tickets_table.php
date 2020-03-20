440f5983a89e82c7b34deaa3366b089a5a091fdc
Yomna-Hesham <yomna.heshamyh@hotmail.com>
2020-03-20 21:36:51 +0200

    migrations, models and controllers

---
+++ database/migrations/2020_03_20_182904_create_tickets_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTicketsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId("user_id")->constrained();
+            $table->foreignId("trip_id")->constrained();
+            $table->foreignId("departure_station_id")->references("id")->on("stations");
+            $table->foreignId("arrival_station_id")->references("id")->on("stations");
+            $table->unsignedInteger("seat");
+            $table->unsignedFloat("price");
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tickets');
+    }
+}
---
+++ database/migrations/2020_03_20_183854_create_trip_stations_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTripStationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trip_stations', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId("station_id")->constrained();
+            $table->foreignId("trip_id")->constrained();
+            $table->unsignedInteger("order");
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trip_stations');
+    }
+}
